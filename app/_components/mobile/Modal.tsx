2d239b6d33676069cce02268d0a443573cb3ffaf
Marcus Vinicius Nascimento Ferreira <marcus.vinicius@peopleinteractive.com.br>
2025-01-08 09:27:56 -0300

    fix/modal: ajuste max width do modal no desktop

--- app/_components/mobile/Modal.tsx
+++ app/_components/mobile/Modal.tsx
@@ -17,7 +17,7 @@ export default function Modal({ children, onClose }: ModalProps) {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-4">
-      <div className="bg-white p-6 rounded-[21px] shadow-lg w-full relative">
+      <div className="bg-white p-6 rounded-[21px] shadow-lg w-full relative md:max-w-96">
         <CloseButton onClick={onClose} />
         {children}
       </div>
